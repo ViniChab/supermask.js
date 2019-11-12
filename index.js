18bd7acaa382f69d2462b8cf764290bb89539dc4
ViniChab <chabvini@gmail.com>
2019-11-11 22:09:54 -0300

    adding number validation

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+"use strinct"
+
 window.onload = () => {
   let maskFields = _getMaskFields()
   let masks = _getMasks(maskFields)
   _setupMaskEvents(maskFields, masks)
-
 }
 
 function _getMaskFields() {
@@ -15,11 +16,33 @@ function _getMasks(maskFields) {
 
 function _setupMaskEvents(maskFields, masks) {
   maskFields.forEach((field, index) => {
-    field.addEventListener('keypress', () => validateMask(field, masks[index]))
+    field.addEventListener('keypress', (event) => validateMask(field, masks[index], event))
   })
 }
 
-function validateMask(maskField, mask) {
-  console.log(maskField)
-  console.log(mask)
+function validateMask(field, mask, event) {
+  let lastDigitPosition = field.value.length
+  let splitMask = mask.split('')
+
+  try {
+    if (!_validateDigit(splitMask[lastDigitPosition], event.key, field))
+      event.preventDefault()
+  } catch (e) {
+    event.preventDefault()
+  }
+}
+
+function _validateDigit(digitMask, key, field) {
+  if (+digitMask)
+    return _numericValidation(digitMask, key)
+  else if (digitMask.match(/[_\W]/)){
+    field.value = field.value.concat(digitMask)
+    return true
+  }
+}
+
+function _numericValidation(digitMask, key) {
+  if (+key <= digitMask) {
+    return true
+  } else return false
 }
