69876e43a9dc55d94b105b974ccfc4fe1e2e7b0e
ViniChab <chabvini@gmail.com>
2019-11-12 23:12:00 -0300

    adding onchange full validation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 String.prototype.isSymbol = function () {
-  return !!this.match(/[_\W]/)
+  return !!this.match(/(?![#])[_\W]/)
 }
 
 window.onload = () => {
@@ -21,6 +21,7 @@ function _getMasks(maskFields) {
 function _setupMaskEvents(maskFields, masks) {
   maskFields.forEach((field, index) => {
     field.addEventListener('keypress', (event) => validateMask(field, masks[index], event))
+    field.addEventListener('change', (event) => fullyValidateMask(field, masks[index], event))
   })
 }
 
@@ -36,11 +37,40 @@ function validateMask(field, mask, event) {
   }
 }
 
-function _validateDigit(splitMask, lastDigitPosition, key, field) {
+function fullyValidateMask(field, mask, event) {
+  let validatedMask = ''
+  try{
+
+    field.value.split('').forEach( (digit, index) => {
+
+      if (_validateDigit(mask.split(''), index, digit, field, true))
+        return validatedMask = validatedMask.concat(digit)
+
+      if (_shouldThisInvalidDigitBeHere(digit, mask[index]))
+        return validatedMask = validatedMask.concat(digit)
+
+      throw 'invalid input'
+
+    })
+
+  } catch(e) {
+    return field.value = validatedMask
+  }
+}
+
+function _shouldThisInvalidDigitBeHere(digit, mask) {
+  return digit == mask
+}
+
+function _validateDigit(splitMask, lastDigitPosition, key, field, isFullValidation = false) {
   let digitMask = splitMask[lastDigitPosition]
 
+  if (key == ' ')
+    return false
+
   if (digitMask.isSymbol()) {
-    field.value = _concatSymbols(field, splitMask, lastDigitPosition)
+    if (!isFullValidation)
+      field.value = _concatSymbols(field, splitMask, lastDigitPosition)
     return _validateDigit(splitMask, (lastDigitPosition + 1), key, field)
   }
 
@@ -55,7 +85,7 @@ function _concatSymbols(field, splitMask, lastDigitPosition) {
   let symbols = ''
   for (let i = lastDigitPosition; i < splitMask.length; i++)
     if (splitMask[i].isSymbol()) symbols = symbols.concat(splitMask[i])
-    else { console.log(splitMask[i]); break }
+    else break 
 
   return field.value.concat(symbols)
 }
