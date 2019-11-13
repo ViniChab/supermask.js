4472db9ba96ae0c4bdbe7ea9b7e0dca8e2680358
ViniChab <chabvini@gmail.com>
2019-11-12 21:46:55 -0300

    solving a bug where special charecter wouldnt care about other validations

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 "use strict"
 
-String.prototype.isSymbol = function() {
+String.prototype.isSymbol = function () {
   return !!this.match(/[_\W]/)
-}
-
-Array.prototype.getNextNonSymbolicValue = function(position) {
-  console.log(this)
-  console.log(position)
 }
 
 window.onload = () => {
@@ -34,7 +29,7 @@ function validateMask(field, mask, event) {
   let splitMask = mask.split('')
 
   try {
-    if (!_validateDigit(splitMask, lastDigitPosition,  event.key, field))
+    if (!_validateDigit(splitMask, lastDigitPosition, event.key, field))
       event.preventDefault()
   } catch (e) {
     event.preventDefault()
@@ -44,21 +39,23 @@ function validateMask(field, mask, event) {
 function _validateDigit(splitMask, lastDigitPosition, key, field) {
   let digitMask = splitMask[lastDigitPosition]
 
-  if (+splitMask.getNextNonSymbolicValue(lastDigitPosition))
+  if (digitMask.isSymbol()) {
+    field.value = _concatSymbols(field, splitMask, lastDigitPosition)
+    return _validateDigit(splitMask, (lastDigitPosition + 1), key, field)
+  }
+
+  if (+digitMask)
     return _numericValidation(digitMask, key, field)
 
-  else if (digitMask == "#") 
+  if (digitMask == "#")
     return true
-
-  else if (digitMask.isSymbol()) 
-    return field.value = _concatSymbols(field, splitMask, lastDigitPosition)
 }
 
 function _concatSymbols(field, splitMask, lastDigitPosition) {
   let symbols = ''
-  for (let i = lastDigitPosition; i < splitMask.length; i++) 
+  for (let i = lastDigitPosition; i < splitMask.length; i++)
     if (splitMask[i].isSymbol()) symbols = symbols.concat(splitMask[i])
-    else break
+    else { console.log(splitMask[i]); break }
 
   return field.value.concat(symbols)
 }
@@ -69,7 +66,7 @@ function _numericValidation(digitMask, key, field) {
       return true
     else return false
 
-  else if (+key <= digitMask) 
+  else if (+key <= digitMask)
     return true
 
   else return false
