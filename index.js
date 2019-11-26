e613c9f1279bf3c64cee34f4100dd9b02ff63b86
ViniChab <chabvini@gmail.com>
2019-11-25 21:53:10 -0300

    Adding final valdiations and removing the HTML file

--- index.js
+++ index.js
@@ -16,64 +16,6 @@ String.prototype.isLetter = function () {
   return !!this.match(/[a-z\A-Z]/)
 }
 
-window.onload = () => {
-  let maskFields = _getMaskFields()
-  let masks = _getMasks(maskFields)
-  _setupMaskEvents(maskFields, masks)
-}
-
-function validateMask(field, mask, event) {
-  let lastDigitPosition = field.value.length
-  let splitMask = mask.split('')
-
-  try {
-    if (!_validateDigit(splitMask, lastDigitPosition, event.key, field))
-      event.preventDefault()
-  } catch (e) {
-    event.preventDefault()
-  }
-}
-
-function fullyValidateChange(field, mask, event) {
-  _addSymbols(field, mask)
-  fullyValidateMask(field, mask, event)
-}
-
-function fullyValidateMask(field, mask, event) {
-  let validatedMask = ''
-  let finalSymbols = _getFinalMaskSymbols(mask)
-
-  try {
-
-    field.value.split('').forEach((digit, index) => {
-
-      if (_validateDigit(mask.split(''), index, digit, field, true))
-        return validatedMask = validatedMask.concat(digit)
-
-      if (_shouldThisInvalidDigitBeHere(digit, mask[index]))
-        return validatedMask = validatedMask.concat(digit)
-
-      throw 'invalid input'
-
-    })
-
-  } catch (e) {
-    return field.value = validatedMask + finalSymbols.join('')
-  }
-}
-
-function _getFinalMaskSymbols(mask) {
-  let finalSymbols = []
-  let reverseSplitMask = mask.split('').reverse()
-  try {
-    reverseSplitMask.forEach(digit => {
-      if (digit.isSymbol()) finalSymbols.push(digit)
-      else throw "Reached a non-symbolic value"
-    })
-  } catch (e) { }
-  return finalSymbols
-}
-
 function _getMaskFields() {
   return Array.from(document.getElementsByClassName('maskField'))
 }
@@ -82,28 +24,17 @@ function _getMasks(maskFields) {
   return maskFields.map(input => input.getAttribute("mask"))
 }
 
-function _addSymbols(field, mask) {
-  if (field.value.length == 0) return field.value
-  let splitMask = mask.split('')
-  let splitValue = field.value.split('')
-
-  try {
-    splitMask.forEach((digit, index) => {
-      if (index > splitValue.length) throw "mask is bigger than input value"
-      if (digit.isSymbol() && splitValue[index] != digit)
-        splitValue = _insertSymbol(splitValue.join(''), index, digit).split('')
-    })
-  } catch (e) { }
-  field.value = splitValue.join('')
+function _shouldThisInvalidDigitBeHere(digit, mask) {
+  return digit == mask
 }
 
-function _insertSymbol(fieldValue, index, symbol) {
-  let splitValue = fieldValue.split('')
-  let firstHalf = fieldValue.substr(0, index)
-  let secondHalf = splitValue.splice(index).join('')
-  return firstHalf + symbol + secondHalf
+window.onload = () => {
+  let maskFields = _getMaskFields()
+  let masks = _getMasks(maskFields)
+  _setupMaskEvents(maskFields, masks)
 }
 
+// Constructor function
 function _setupMaskEvents(maskFields, masks) {
   maskFields.forEach((field, index) => {
     field.addEventListener('input', (event) => {
@@ -135,6 +66,87 @@ function _setupMaskEvents(maskFields, masks) {
   })
 }
 
+// Function to validate mask on type, it'll also add symbols
+function validateMask(field, mask, event) {
+  let lastDigitPosition = field.value.length
+  let splitMask = mask.split('')
+
+  try {
+    if (!_validateDigit(splitMask, lastDigitPosition, event.key, field))
+      event.preventDefault()
+  } catch (e) {
+    event.preventDefault()
+  }
+}
+
+// Capsule function to validate field value after focusout
+function fullyValidateChange(field, mask, event) {
+  _addSymbols(field, mask)
+  fullyValidateMask(field, mask, event)
+}
+
+// Function to validate every digit
+function fullyValidateMask(field, mask, event) {
+  let validatedMask = ''
+  let finalSymbols = _getFinalMaskSymbols(mask)
+
+  try {
+
+    field.value.split('').forEach((digit, index) => {
+
+      if (_validateDigit(mask.split(''), index, digit, field, true))
+        return validatedMask = validatedMask.concat(digit)
+
+      if (_shouldThisInvalidDigitBeHere(digit, mask[index]))
+        return validatedMask = validatedMask.concat(digit)
+
+      throw 'invalid input'
+
+    })
+
+  } catch (e) {
+    return field.value = validatedMask + finalSymbols.join('')
+  }
+}
+
+// Function used to catch any symbols a mask might have at the end (such as AAA---)
+function _getFinalMaskSymbols(mask) {
+  let finalSymbols = []
+  let reverseSplitMask = mask.split('').reverse()
+  try {
+    reverseSplitMask.forEach(digit => {
+      if (digit.isSymbol()) finalSymbols.push(digit)
+      else throw "Reached a non-symbolic value"
+    })
+  } catch (e) { }
+  return finalSymbols
+}
+
+// Will add every symbol the mask should have
+function _addSymbols(field, mask) {
+  if (field.value.length == 0) return field.value
+  let splitMask = mask.split('')
+  let splitValue = field.value.split('')
+
+  try {
+    splitMask.forEach((digit, index) => {
+      if (index > splitValue.length) throw "mask is bigger than input value"
+      if (digit.isSymbol() && splitValue[index] != digit)
+        splitValue = _insertSymbol(splitValue.join(''), index, digit).split('')
+    })
+  } catch (e) { }
+  field.value = splitValue.join('')
+}
+
+// Function used to insert symbol at specific position
+function _insertSymbol(fieldValue, index, symbol) {
+  let splitValue = fieldValue.split('')
+  let firstHalf = fieldValue.substr(0, index)
+  let secondHalf = splitValue.splice(index).join('')
+  return firstHalf + symbol + secondHalf
+}
+
+//Function to make backspace unable to delete a symbol at the middle of the field
 function _validateBackspaceButton(field, mask, deletionType = "backspace") {
   let caretPosition = _getCaretPos(field)
   let deletedCharacter = mask[caretPosition]
@@ -155,10 +167,7 @@ function _validateBackspaceButton(field, mask, deletionType = "backspace") {
   }
 }
 
-function _shouldThisInvalidDigitBeHere(digit, mask) {
-  return digit == mask
-}
-
+// Will validate a specific digit of the mask
 function _validateDigit(splitMask, lastDigitPosition, key, field, isFullValidation = false) {
   let digitMask = splitMask[lastDigitPosition]
 
@@ -173,7 +182,7 @@ function _validateDigit(splitMask, lastDigitPosition, key, field, isFullValidati
     return _validateDigit(splitMask, (lastDigitPosition + 1), key, field, true)
   }
 
-  if (+digitMask)
+  if (+digitMask || digitMask == "0")
     return _numericValidation(digitMask, key, field)
 
   if (digitMask.isUpperCaseLetter() && key.isUpperCaseLetter())
@@ -190,6 +199,7 @@ function _validateDigit(splitMask, lastDigitPosition, key, field, isFullValidati
 
 }
 
+// Function to concat symbols at the end of mask
 function _concatSymbols(field, splitMask, lastDigitPosition) {
   let symbols = ''
   for (let i = lastDigitPosition; i < splitMask.length; i++)
@@ -197,18 +207,6 @@ function _concatSymbols(field, splitMask, lastDigitPosition) {
     else break
 
   return field.value.concat(symbols)
-}
-
-function _numericValidation(digitMask, key, field) {
-  if (field.hasAttribute("inverted-numbers"))
-    if (+key >= digitMask)
-      return true
-    else return false
-
-  else if (+key <= digitMask)
-    return true
-
-  else return false
 }
 
 function _setCaretPosition(field, caretPos) {
@@ -236,5 +234,14 @@ function _getCaretPos(field) {
   return caret_pos
 }
 
-// TODO adicionar letra na máscara
-// TODO adicionar uns comentários nas funções
+function _numericValidation(digitMask, key, field) {
+  if (field.hasAttribute("inverted-numbers"))
+    if (+key >= digitMask)
+      return true
+    else return false
+
+  else if (+key <= digitMask)
+    return true
+
+  else return false
+}
